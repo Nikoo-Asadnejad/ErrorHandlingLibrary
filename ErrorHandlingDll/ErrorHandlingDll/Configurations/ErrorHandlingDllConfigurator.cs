02853cf5aaf29a3be34cea43db78fa62cd00724f
= <91413713+Nikoo-Asadnejad@users.noreply.github.com>
2022-06-04 23:41:20 +0430

    Chores : add packages

---
+++ ErrorHandlingDll/ErrorHandlingDll/Configurations/ErrorHandlingDllConfigurator.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandlingDll.Configurations
+{
+    public static class ErrorHandlingDllConfigurator
+    {
+    public static void InjectServices(IServiceCollection services)
+    {
+
+    }
+
+    //public static void ConfigureAppPipeline(WebApplication app)
+    //{
+
+    //}
+
+  }
+}
